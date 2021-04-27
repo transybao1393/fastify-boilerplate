0534f275e537f83e7efd8b38c82331c562546051
Huu Thi <nguyenhuuthi@mecury.com.vn>
2021-04-27 17:45:25 +0700

    Add sequelize

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,22 +1,19 @@
-var Pool = require('pg-pool')
+const { Sequelize } = require('sequelize');
 
 function connection() {
-    var client = new Pool({
-        database: 'bluefin',
-        user: 'sbs',
-        password: 'CpZrKPUuQZYzVHTchPkQ',
-        port: 5432,
-        max: 20, // set pool max size to 20
-        idleTimeoutMillis: 1000, // close idle clients after 1 second
-        connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-        maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
-    })
-    
-    return client.connect().then(db=>{
-        console.log("connect db success");
-    }).catch(err=> {
-        console.log("connect db fail", err);
-    });
+    try {
+        return new Sequelize('database', 'username', 'password', {
+            host: 'localhost',
+            dialect: 'postgres',
+            pool: {
+              max: 5,
+              min: 0,
+              idle: 10000
+            },
+        });
+    } catch(error) {
+        console.log("Connect db fail");
+    }
 }
 
 module.exports = connection();
